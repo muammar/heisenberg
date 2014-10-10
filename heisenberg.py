c5e4a13691ecd9c28ff4c16c7207fd83c9fb165a
Muammar El Khatib <muammarelkhatib@gmail.com>
2014-10-10 17:52:19 +0200

    Welcome message, and different states are printed

--- heisenberg.py
+++ heisenberg.py
@@ -11,10 +11,31 @@ j2=1.0   # This is the RATIO j2/j1 (low value high dimerization)
 n=float(nelec)
 bc='obc'
 ms=0
+nstate=2    #Number of states to print
 preket=list(it.product([0.5,-0.5], repeat=nelec))
 prebra=list(it.product([1,0], repeat=nelec))
-print prebra
+#### Commented on 10/10/2014 print prebra
 # Convert list of tuples to list of lists
+
+welcome = """\
+-------------------------------------------------------------------------------
+
+
+██╗  ██╗███████╗██╗███████╗███████╗███╗   ██╗██████╗ ███████╗██████╗  ██████╗
+██║  ██║██╔════╝██║██╔════╝██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗██╔════╝
+███████║█████╗  ██║███████╗█████╗  ██╔██╗ ██║██████╔╝█████╗  ██████╔╝██║  ███╗
+██╔══██║██╔══╝  ██║╚════██║██╔══╝  ██║╚██╗██║██╔══██╗██╔══╝  ██╔══██╗██║   ██║
+██║  ██║███████╗██║███████║███████╗██║ ╚████║██████╔╝███████╗██║  ██║╚██████╔╝
+╚═╝  ╚═╝╚══════╝╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝
+
+A program to calculate the total position spread tensor using a Heisenberg
+hamiltonian.
+
+This program has been written by Muammar El Khatib and Edoardo Fertitta.
+-------------------------------------------------------------------------------
+"""
+
+print (welcome)
 
 """
 Ms partition
@@ -24,16 +45,19 @@ for idx,i in enumerate(preket):
     if np.sum(i) == ms:
         bra.append(prebra[idx])
 
-print bra
+#### Commented on 10/10/2014 print bra
+
 brams=[]
 for i in bra:
     brams.append(np.sum(i))
 
 
-print brams
+#### Commented on 10/10/2014 print brams
 
+print ('Heisenberg hamiltonian matrix:')
+print ('')
 ovm = np.zeros(shape=(len(bra),len(bra)))
-print bra
+#### Commented on 10/10/2014 print bra
 for idx,i in enumerate(bra):
     for idy,j in enumerate(bra):
         if brams[idx] == brams[idy]:
@@ -43,7 +67,7 @@ for idx,i in enumerate(bra):
 #                print diff
                  diffn= np.squeeze(np.asarray(diff))
                  order=np.flatnonzero(diffn)
-                 print order
+                 #### Commented on 10/10/2014 print order
                  if bc == 'pbc':
                      if order[1]-order[0] == 1 or  order[1]-order[0] == nelec-1: #PBC
                          ovm.itemset((idx,idy),1)
@@ -92,35 +116,40 @@ for idd,i in enumerate(prediagonal):
     ovm.itemset((idd,idd),j)
 
 print ovm
+print ('')
 
 from scipy import linalg as LA
 e_vals, e_vecs = LA.eigh(ovm)
 
-for idx, i in enumerate(e_vals):
-    print e_vals[idx]
-    print e_vecs[:,idx]
-    print ''
+"""
+This is let just for debugging reasons.
+"""
+#for idx, i in enumerate(e_vals):
+#    print e_vals[idx]
+#    print e_vecs[:,idx]
+#    print ''
 
-gsev=e_vecs[:,0]
+enumerate(e_vals)
 
 np.set_printoptions(precision=8,suppress=True)
-print ('Print ground state vector')
-print gsev
 
-idxtomatch=np.flatnonzero(gsev)
-
-presumtps=[]
-for i in idxtomatch:
-#   print bra[i]
-    brarray=np.array(bra[i])
-    positions=(np.flatnonzero(brarray)).astype(float)-(n-1.)/2.
-#   print positions
-    sq=np.sum(positions)**2
-    coefsq=gsev[i]**2*sq
-#   print coefsq
-    presumtps.append(coefsq)
-
-#print presumtps
-print ('')
-print ('Λ(αα) contribution = ' + str(np.sum(presumtps)))
-#print ('Λ(αα+ββ) contrib = ' + str(np.sum(presumtps)))
+for state in range(nstate):
+    print ('Results for STATE '+str(state+1)+': ')
+    print ('Eigenvalue= '+ str(e_vals[state]))
+    print ('Corresponding state vector:')
+    print e_vecs[:,state]
+    idxtomatch=np.flatnonzero(e_vecs[:,state])
+    presumtps=[]
+    for i in idxtomatch:
+        #   print bra[i]
+        brarray=np.array(bra[i])
+        positions=(np.flatnonzero(brarray)).astype(float)-(n-1.)/2.
+        #   print positions
+        sq=np.sum(positions)**2
+        coefsq=e_vecs[:,state][i]**2*sq
+        #   print coefsq
+        presumtps.append(coefsq)
+    #print presumtps
+    print ('Λ(αα) contribution = ' + str(np.sum(presumtps)))
+    print ('')
+    #print ('Λ(αα+ββ) contrib = ' + str(np.sum(presumtps)))
