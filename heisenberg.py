04042dfd9b48c35c18277dd19a22c62070c14cd8
Muammar El Khatib <muammarelkhatib@gmail.com>
2015-08-27 13:42:11 +0200

    - License matters updated.

--- heisenberg.py
+++ heisenberg.py
@@ -4,11 +4,34 @@
 __author__ = "Muammar El Khatib; Edoardo Fertitta."
 __copyright__ = "Copyright 2014, Muammar El Khatib; Edoardo Fertitta."
 __credits__ = [""]
-__license__ = "GPL"
-__version__ = "3"
+__license__ = "MIT"
+__version__ = ""
 __maintainer__ = "Muammar El Khatib"
 __email__ = "muammarelkhatib@gmail.com"
 __status__ = "Development"
+
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Muammar El Khatib; Edoardo Fertitta.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 welcome = """\
